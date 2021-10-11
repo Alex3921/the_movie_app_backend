beecf70d8eeb6c5681860e2301e324e04684000e
alexandru <boncut.alex10@gmail.com>
2021-10-11 17:43:56 -0500

    Add custom action 'search'

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    movies = Movie.all
+    movies = Movie.call
     render json: movies
+  end
+
+  def search
+    query = request.original_url.split('/').last
+    response = Movie.search(query)
+    render json: response
   end
 end

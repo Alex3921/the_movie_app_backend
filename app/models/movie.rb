da72cdc1071817b5962d9b0dd8826139e69d09ec
alexandru <boncut.alex10@gmail.com>
2021-10-11 17:44:30 -0500

    Add methods: call and search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   has_many :reviews
+
+  def self.call
+    HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&sort_by=popularity.desc&include_video=true&page=1&primary_release_year=2020")
+  end
+
+  def self.search(query)
+    HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{query}&page=1&include_adult=false")
+  end
 end

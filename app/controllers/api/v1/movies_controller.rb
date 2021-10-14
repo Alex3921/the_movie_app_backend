861aac17e555aa939ea63e7f9bb2c1b409ce2651
alexandru <boncut.alex10@gmail.com>
2021-10-14 13:42:22 -0500

    Add action: create

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,9 +4,36 @@ class Api::V1::MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    byebug
+    if movie.save
+      render json: movie, status: :created
+    else
+      render json: {error: "Could not save this movie"}
+    end
+  end
+
   def search
     query = request.original_url.split('/').last
     response = Movie.search(query)
     render json: response
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(
+      :title, 
+      :overview, 
+      :poster_path, 
+      :vote_average, 
+      :external_id, 
+      reviews_attributes: 
+        [
+          :author_name, 
+          :content, 
+          :stars, 
+          :movie_id
+        ]
+      )
   end
 end

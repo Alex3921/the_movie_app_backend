0a84b52ee2cdc54eea6bb756ddec4a892eb1dbbb
alexandru <boncut.alex10@gmail.com>
2021-10-15 13:37:03 -0500

    Update search method
    The method looks into our db for the query

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,10 +3,13 @@ class Movie < ApplicationRecord
   accepts_nested_attributes_for :reviews
 
   def self.call
-    HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&sort_by=popularity.desc&include_video=true&page=1&primary_release_year=2020")
+    HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&sort_by=popularity.desc&page=1")
   end
 
   def self.search(query)
-    HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{query}&page=1&include_adult=false")
+    title = Movie.arel_table[:title]
+    Movie.where(title.matches("%#{query}%"))
+    # HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{query}&page=1&include_adult=false")
   end
 end
+

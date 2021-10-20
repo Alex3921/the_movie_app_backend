4752ea3fe714d26cc8c2bead9d15283478b1aab4
alexandru <boncut.alex10@gmail.com>
2021-10-19 19:33:50 -0500

    Add actions: create and destroy

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,22 @@
-class Api:V1::ReviewsController < ApplicationController
+class Api::V1::ReviewsController < ApplicationController
+    def create
+        review = Review.new(review_params)
+        if review.save
+            render json: review, status: :created
+        else
+            render json: { error: "There was an error. Please try again!" }
+        end
+    end
+
+    def destroy
+        review = Review.find_by_id(params[:id])
+        review.destroy
+        render json: { message: "Review was successfully removed" }
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:author_name, :content, :stars, :movie_id)
+    end
+
 end

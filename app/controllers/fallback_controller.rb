0814ceb4ea10254b2c49a598f63160b576403f11
Alexandru Boncut <boncut.alex10@gmail.com>
2022-03-05 13:23:12 -0600

    Update fallback url

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,5 @@
 class FallbackController < ActionController::Base
     def index
-        redirect_back(fallback_location: "http://the-movie-app.heroku.com/api/v1/movies", status: 404)
+        redirect_back(fallback_location: "http://the-movie-app-backend.heroku.com/api/v1/movies", status: 404)
     end
 end

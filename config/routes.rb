47d844e1fa70e5093db5029647eec3cf1eebfb69
Alexandru Boncut <boncut.alex10@gmail.com>
2022-03-06 14:04:14 -0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'api/v1/movies#index'
   
   namespace :api do
     namespace :v1 do
       
       resources :reviews, only: [:create, :destroy]
-
-      get 'movies', to: 'movies#index'
+      
       get 'movies/search/:query', to: 'movies#search'
       get 'movies/:id', to: 'movies#show'
     end
